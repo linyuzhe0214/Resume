9dbc10f514f5f9b752932d5619363152f189e122
linyuzhe0214 <linyuzhe02140@gmail.com>
2026-06-04 20:18:35 +0800

    feat: create RampHistory component for viewing and managing ramp maintenance segments

--- src/components/RampHistory.tsx
+++ src/components/RampHistory.tsx
@@ -852,6 +852,7 @@ export default function RampHistory(props: RampHistoryProps) {
                           </div>
                         );
                       })}
+                    </div>
                   </div>
                 </div>
                 );
